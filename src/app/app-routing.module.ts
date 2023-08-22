ca10c1692aab71a10d9aeec966c4489f72594a2e
Léo Hendryc <hendrycvs@gmail.com>
2023-08-22 17:16:02 -0300

    adicionado os modulos e criado os routers com um menuprovisorio porem funcional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ConsultarComponent } from './components/consultar/consultar.component';
+import { CadastrarComponent } from './components/cadastrar/cadastrar.component';
+import { EntradaComponent } from './components/entrada/entrada.component';
+import { SaidaComponent } from './components/saida/saida.component';
 
-
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "consultar", component: ConsultarComponent },
+  { path: "cadastrar", component: CadastrarComponent },
+  { path: "entrada", component: EntradaComponent },
+  { path: "saida", component: SaidaComponent }
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ConsultarComponent } from './components/consultar/consultar.component';
+import { CadastrarComponent } from './components/cadastrar/cadastrar.component';
+import { EntradaComponent } from './components/entrada/entrada.component';
+import { SaidaComponent } from './components/saida/saida.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultarComponent,
+    CadastrarComponent,
+    EntradaComponent,
+    SaidaComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
